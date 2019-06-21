599404a0d2088e81fc05a416b4619f8d845cd551
Cyrille David <cyrille@qonto.eu>
2019-06-21 12:39:26 +0200

    Create the plugin

---
+++ index.js
@@ -0,0 +1,37 @@
+const Rule = require("ember-template-lint").Rule;
+
+const PrettierRule = class Prettier extends Rule {
+  visitor() {
+    return {
+			// TODO: turn this example into a real thing
+      // CommentStatement(node) {
+      //   if (node.value.trim() === "") {
+      //     this.log({
+      //       message: "comments cannot be empty",
+      //       line: node.loc && node.loc.start.line,
+      //       column: node.loc && node.loc.start.column,
+      //       source: this.sourceForNode(node)
+      //     });
+      //   }
+      // }
+    };
+  }
+};
+
+module.exports = {
+  name: "prettier",
+
+  rules: {
+    prettier: PrettierRule
+  },
+
+  // Define configurations for this plugin that can be extended by the base configuration
+  configurations: {
+    recommended: {
+      plugins: ["prettier"],
+      rules: {
+        prettier: true
+      }
+    }
+  }
+};
