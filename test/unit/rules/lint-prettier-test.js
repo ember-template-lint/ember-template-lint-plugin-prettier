0da85c70bd2f3b798cb07b8deddd7e8e37ef241f
Cyrille David <dcyriller@gmail.com>
2019-06-23 16:04:02 +0200

    Make first two tests pass
    Note that there is a PR opened on Prettier to add a trailing newline to
    hbs files.
    
    So, once that will be merged this test should be reversed :wink:

--- test/unit/rules/lint-prettier-test.js
+++ test/unit/rules/lint-prettier-test.js
@@ -3,87 +3,30 @@ const generateRuleTests = require('../../helpers/rule-test-harness');
 generateRuleTests({
   name: 'prettier',
 
-  good: [
-    {
-      config: 'always',
-      template: 'test\n',
-    },
-    {
-      config: 'never',
-      template: 'test\n',
-    },
-    // test the re-entering of yielded content
-    {
-      config: 'never',
-      template: '{{#my-component}}\n\n\n' + '  test\n\n\n' + '{{/my-component}}',
-    },
-    {
-      config: 'always',
-      template: '{{#my-component}}{{/my-component}}\n',
-    },
-  ],
+  good: ['test', '{{#my-component}}{{/my-component}}'],
 
-  bad: [
-    // {
-    //   config: 'always',
-    //   template: 'test',
+  // bad: [
+  //   {
+  //     template: 'test\n',
 
-    //   result: {
-    //     moduleId: 'layout.hbs',
-    //     message: 'template must end with newline',
-    //     line: 1,
-    //     column: 0,
-    //     source: 'test',
-    //   },
-    // },
-    // {
-    //   config: 'never',
-    //   template: 'test\n',
+  //     result: {
+  //       moduleId: 'layout.hbs',
+  //       message: 'template must not end with newline',
+  //       line: 1,
+  //       column: 0,
+  //       source: 'test',
+  //     },
+  //   },
+  //   {
+  //     template: '{{#my-component}}\n\n\n' + '  test\n\n\n' + '{{/my-component}}',
 
-    //   result: {
-    //     moduleId: 'layout.hbs',
-    //     message: 'template cannot end with newline',
-    //     line: 1,
-    //     column: 0,
-    //     source: 'test\n',
-    //   },
-    // },
-    // // test the re-entering of yielded content
-    // // only generates one error instead of two
-    // {
-    //   config: 'never',
-    //   template: '{{#my-component}}\n' + '  test\n' + '{{/my-component}}\n',
-
-    //   result: {
-    //     moduleId: 'layout.hbs',
-    //     message: 'template cannot end with newline',
-    //     line: 1,
-    //     column: 0,
-    //     source: '{{#my-component}}\n' + '  test\n' + '{{/my-component}}\n',
-    //   },
-    // },
-  ],
-
-  error: [
-    // {
-    //   config: 'sometimes',
-    //   template: 'test',
-
-    //   result: {
-    //     fatal: true,
-    //     moduleId: 'layout.hbs',
-    //     message: 'You specified `"sometimes"`',
-    //   },
-    // },
-    // {
-    //   config: true,
-    //   template: 'test',
-
-    //   result: {
-    //     fatal: true,
-    //     moduleId: 'layout.hbs',
-    //     message: 'You specified `true`',
-    //   },
-    // },
-  ],
+  //     result: {
+  //       moduleId: 'layout.hbs',
+  //       message: 'template cannot end with newline',
+  //       line: 1,
+  //       column: 0,
+  //       source: '{{#my-component}}\n' + '  test\n' + '{{/my-component}}\n',
+  //     },
+  //   },
+  // ],
 });
