285a86b4b8208c6c4d4a02448c48a1db89e7d26f
Cyrille David <cyrille@qonto.eu>
2019-11-24 16:50:12 +0100

    Add a recommended set
    It allows prettier rule. It disables ember-template-lint sylistic rules.

--- ember-template-lint-plugin-prettier.js
+++ ember-template-lint-plugin-prettier.js
@@ -3,6 +3,12 @@ const PrettierRule = require("./lib/rules/prettier");
 module.exports = {
   name: "ember-template-lint-plugin-prettier",
 
+  configurations: {
+    recommended: {
+      rules: require("./lib/config/recommended")
+    }
+  },
+
   rules: {
     prettier: PrettierRule
   }
